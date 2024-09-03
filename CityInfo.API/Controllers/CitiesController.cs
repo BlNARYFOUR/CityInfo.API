d6c954d66aaa17da3ae056c82e87f696c84eff8b
BlNARYFOUR <lambertbrend@gmail.com>
2024-09-03 16:26:39 +0200

    Show basic city by id & refactor code

--- CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CityInfo.API.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CityInfo.API.Controllers
 {
@@ -7,9 +9,22 @@ namespace CityInfo.API.Controllers
     public class CitiesController : ControllerBase
     {
         [HttpGet]
-        public JsonResult GetCities()
+        public ActionResult<IEnumerable<CityDto>> GetCities()
         {
-            return new JsonResult(CitiesDataStore.GetInstance().Cities);
+            return Ok(CitiesDataStore.GetInstance().Cities);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<CityDto> GetCity(int id)
+        {
+            CityDto? city = CitiesDataStore.GetInstance().Cities.FirstOrDefault(c => c.Id == id);
+
+            if (null == city)
+            {
+                return NotFound();
+            }
+
+            return Ok(city);
         }
     }
 }
