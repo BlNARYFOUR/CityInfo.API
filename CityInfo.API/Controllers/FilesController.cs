f79bd5edc2daa30500583d6208b495db46726eb8
BlNARYFOUR <lambertbrend@gmail.com>
2024-09-04 12:07:44 +0200

    Get files by id

---
+++ CityInfo.API/Controllers/FilesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/files")]
+    [ApiController]
+    public class FilesController(
+        FileExtensionContentTypeProvider fileExtensionContentTypeProvider
+    ) : ControllerBase {
+        private readonly FileExtensionContentTypeProvider _fileExtensionContentTypeProvider = fileExtensionContentTypeProvider
+            ?? throw new System.ArgumentNullException(nameof(fileExtensionContentTypeProvider));
+
+        [HttpGet("{id}")]
+        public ActionResult GetFile(int id)
+        {
+            string[] paths = Directory.GetFiles("Assets/", "file-" + id.ToString("000") + ".*");
+
+            if (0 == paths.Length)
+            {
+                return NotFound();
+            }
+
+            string path = paths[0];
+
+            if (!_fileExtensionContentTypeProvider.TryGetContentType(path, out string? contentType))
+            {
+                contentType = "application/octet-stream";
+            }
+
+            return File(System.IO.File.ReadAllBytes(path), contentType, Path.GetFileName(path));
+        }
+    }
+}
