01faf7057a2abd121c16b00f9551ea369ca55de9
BlNARYFOUR <lambertbrend@gmail.com>
2024-09-04 16:53:38 +0200

    Validate creation of point of interest

--- CityInfo.API/Models/PointOfInterestDto.cs
+++ CityInfo.API/Models/PointOfInterestDto.cs
@@ -1,10 +1,15 @@
-﻿namespace CityInfo.API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CityInfo.API.Models
 {
     public class PointOfInterestDto
     {
         private static int _id = 1;
         public int Id { get; set; } = _id++;
+        [Required]
+        [MaxLength(64)]
         public string Name { get; set; } = string.Empty;
+        [MaxLength(256)]
         public string? Description { get; set; }
     }
 }
