0f4588603c0ec9852f05613b944d9ad1ad47902a
BlNARYFOUR <lambertbrend@gmail.com>
2024-09-05 14:47:46 +0200

    Delete a point of interest

---
+++ CityInfo.API/Controllers/PointsOfInterest/DeleteController.cs
@@ -0,0 +1,32 @@
+﻿using CityInfo.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers.PointsOfInterest
+{
+    [Route("api/cities/{cityId}/pointsofinterest/{id}")]
+    [ApiController]
+    public class DeleteController : ControllerBase
+    {
+        [HttpDelete]
+        public ActionResult<PointOfInterestDto> DeletePointOfInterest(int cityId, int id)
+        {
+            CityDto? city = CitiesDataStore.Instance.Cities.FirstOrDefault(c => cityId == c.Id);
+
+            if (null == city)
+            {
+                return NotFound();
+            }
+
+            PointOfInterestDto? pointOfInterestToDelete = city.PointsOfInterest.FirstOrDefault(p => p.Id == id);
+
+            if (null == pointOfInterestToDelete)
+            {
+                return NotFound();
+            }
+
+            city.PointsOfInterest.Remove(pointOfInterestToDelete);
+
+            return NoContent();
+        }
+    }
+}
